8b122adb88c99f26460e69e39f514b9857f763ad
marksard <marksard@rocketmail.com>
2024-08-09 23:28:48 +0900

    move patternseq

--- app/RhythmBox/src/PatternSeq.hpp
+++ app/RhythmBox/src/PatternSeq.hpp
@@ -8,3 +8,146 @@
 #pragma once
 #include <Arduino.h>
 #include <U8g2lib.h>
+
+#define SEQUENCER_TOTAL 6
+#define STEP_MAX 16
+#define STEP_TOTAL 64 // 16 * 4
+#define BEATS_TOTAL 41
+
+// 0:休 1:打
+// 1以上:4小節単位での休打切り替え。例：0b1010(10)→8小節目、16小節目
+const static uint8_t beats[BEATS_TOTAL][STEP_MAX]
+{
+    {1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1}, // 0
+    {1, 0, 1, 0,  1, 0, 1, 0,  1, 0, 1, 0,  1, 0, 1, 0},
+    {1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0},
+    {0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0},
+    {0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0},
+    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0},
+    {0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},
+    {0, 1, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},
+    {0, 0, 0, 0,  0, 0, 0, 1,  0, 1, 0, 0,  0, 0, 0, 0},
+    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 1, 0, 1},
+
+    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0}, // 10
+    {1, 0, 1, 0,  1, 0, 1,10,  1,10, 1, 0,  1, 0, 1, 0},
+    {1, 0, 1, 0,  1, 0, 1, 0,  1, 0, 1, 8,  1, 8, 1, 8},
+    {0, 0, 1, 0,  0, 0, 1, 0,  0,10, 1, 0,  0, 8, 1, 0},
+    {0, 8, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  2, 0, 1, 0},
+    {0, 0, 1, 0,  0, 1, 0, 8,  0, 0, 1, 0,  0, 8, 1, 0},
+    {0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1,10},
+    {1, 0, 1,10,  1, 0,10, 1,  0, 1, 0, 0,  1, 8, 0, 1},
+    {0, 1, 0, 1,  0, 1, 0, 1,  0, 1, 0, 1,  0, 1,10, 1},
+    {0, 1, 0, 1,  1,10, 0, 1,  0, 1, 1, 0,  1, 8, 0, 1},
+
+    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0}, // 20
+    {0, 0, 0, 0,  1, 0, 0, 1,  0, 1, 0, 0,  1, 0, 0, 0},
+    {0, 0, 0, 0,  1, 0, 0, 1,  0, 8, 0, 0,  1, 0, 0, 0},
+    {0, 0, 0, 0,  1, 0, 0, 1,  0, 8, 0, 0,  1, 0, 0,10},
+    {0, 8, 0, 0,  1, 0, 0,10,  0, 8, 0, 0,  1, 0, 0, 0},
+    {0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0,10},
+    {0, 8, 0, 0,  0, 0, 8, 0,  0, 8, 0, 0,  0, 0,10, 0},
+    {0, 0,10, 0,  0, 8,10, 0,  0, 8, 0,10,  2, 8, 0,10},
+    {0, 0, 0, 0,  0, 0, 0,10,  0,10,10, 0,  0, 0, 0,10},
+    {0, 8, 0, 0,  0, 0, 0,10,  8,10, 0, 0,  0, 0, 8,10},
+
+    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0}, // 30
+    {1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0,10},
+    {1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 8, 0},
+    {1, 0, 1, 0,  0, 0, 1, 0,  0, 0, 0, 1,  0, 0,10, 0},
+    {1, 0, 8, 0,  0, 0, 0, 0,  0, 0, 1, 0,  0,10, 0, 0},
+    {1, 0,10, 0,  0, 0, 0, 0,  0, 0, 1, 8,  0, 0, 0, 0},
+    {1, 0, 0, 1,  0, 8, 0, 0,  0, 0, 1, 0,  0,10, 0, 8},
+    {1, 0, 0, 1,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0,10, 0},
+    {1, 0, 0, 1,  1, 0, 1, 0,  1, 0, 1, 0,  0, 0,10, 8},
+    {1, 0, 0, 1,  0, 0, 1, 0,  0, 1, 0, 0,  1, 0, 0, 8},
+
+    {1, 0, 1, 0,  0, 1, 0, 0,  1, 0, 0, 1,  0, 0, 1, 8}, // 40
+};
+
+struct PatternSetting
+{
+public:
+    PatternSetting()
+    {
+        
+    }
+public:
+    char disp_name[3] = {0};
+    int16_t currentPattern;
+};
+
+class PatternSeq
+{
+public:
+    PatternSeq()
+    {
+
+    }
+
+    void updateDisplay(U8G2 *pU8g2, int8_t currentStep, int8_t selector, int8_t mode)
+    {
+        uint8_t origin_x = 15;
+        uint8_t origin_y = 15;
+        uint8_t seqXStep = 7;
+        uint8_t seqYStep = 8;
+
+        pU8g2->setFont(u8g2_font_7x14B_tf);
+        pU8g2->drawStr(0, 0, "PATTERN SEQ");
+        pU8g2->setFont(u8g2_font_5x8_tf);
+
+        for (int8_t i = 0; i < SEQUENCER_TOTAL + 1; ++i)
+        {
+            pU8g2->drawStr(0, origin_y + (seqYStep * i), patternSetting[i].disp_name);
+            pU8g2->drawHLine(origin_x, origin_y + (seqYStep * i), 127);
+        }
+
+        for (int8_t i = 0; i < STEP_MAX + 1; ++i)
+        {
+            pU8g2->drawVLine(origin_x + (seqXStep * i), origin_y, 63);
+        }
+
+        for (int8_t x = 0; x < STEP_MAX; ++x)
+        {
+            for (int8_t y = 0; y < SEQUENCER_TOTAL; ++y)
+            {
+                uint8_t beat = beats[patternSetting[y].currentPattern][x];
+                if((beat >> 1) & 1) beat = 2;
+                else if((beat >> 2) & 1) beat = 3;
+                else if((beat >> 3) & 1) beat = 4;
+                int8_t size = 6 - beat;
+                int8_t offset = 1;
+                if (size < 6)
+                    pU8g2->drawBox((origin_x + offset) + (seqXStep * x), 
+                                (origin_y + offset) + (seqYStep * y), 
+                                size, size);
+            }
+        }
+
+        pU8g2->drawBox(origin_x + (seqXStep * currentStep), origin_y - 2, seqXStep, 4);
+        
+        if (mode == 0)
+            pU8g2->drawBox(10, (origin_y + 1) + (seqYStep * (selector % SEQUENCER_TOTAL)), 4, 6);
+        else
+            pU8g2->drawStr(10, (origin_y + 1) + (seqYStep * (selector % SEQUENCER_TOTAL)), ">");
+    }
+
+    void addSelectPattern(uint8_t seqIndex, int8_t value)
+    {
+        patternSetting[seqIndex].currentPattern = constrain(patternSetting[seqIndex].currentPattern + value, 0, (BEATS_TOTAL - 1));
+    }
+
+    uint8_t getBeat(uint8_t seqIndex, uint8_t step)
+    {
+        return beats[patternSetting[seqIndex].currentPattern][step & (STEP_MAX - 1)];
+    }
+
+    void setPattern(uint8_t seqIndex, uint8_t value) { patternSetting[seqIndex].currentPattern = value; }
+    void setPatternName(uint8_t seqIndex, const char* value)
+    {
+        strncpy(patternSetting[seqIndex].disp_name, value, 2);
+    }
+
+private:
+    PatternSetting patternSetting[SEQUENCER_TOTAL];
+};
--- app/RhythmBox/src/main.cpp
+++ app/RhythmBox/src/main.cpp
@@ -17,6 +17,7 @@
 #include "../../commonlib/ui_common/SettingItem.hpp"
 #include "../../commonlib/common/epmkii_gpio.h"
 #include "../../commonlib/common/pwm_wrapper.h"
+#include "PatternSeq.hpp"
 #include "SingleShotWave.hpp"
 
 #include "wavetable/909_01/909_BD_Norm.h"
@@ -32,9 +33,8 @@ SingleShotWave LT(buf_909_LT, buf_size_909_LT);
 SingleShotWave RS(buf_909_RS, buf_size_909_RS);
 SingleShotWave SD(buf_909_SD_Snap, buf_size_909_SD_Snap);
 SingleShotWave BD(buf_909_BD_Norm, buf_size_909_BD_Norm);
-#define VOICE_MAX 6
-SingleShotWave<int16_t> *pKit[VOICE_MAX] = { &RC, &OH, &LT, &RS, &SD, &BD };
-int pwmOuts[VOICE_MAX] = { OUT1, OUT2, OUT3, OUT4, OUT5, OUT6 };
+SingleShotWave<int16_t> *pKit[SEQUENCER_TOTAL];
+int pwmOuts[SEQUENCER_TOTAL] = { OUT1, OUT2, OUT3, OUT4, OUT5, OUT6 };
 bool isSDFill = false;
 
 #define CPU_CLOCK 133000000.0
@@ -55,9 +55,9 @@ static SmoothAnalogRead cv1;
 static SmoothAnalogRead cv2;
 
 // setting values
-float pitches[VOICE_MAX] = {1.0,1.0,1.0,1.0,1.0,1.0};
-float decays[VOICE_MAX] = {1.0,1.0,1.0,1.0,1.0,1.0};
-float volumes[VOICE_MAX] = {1.0,1.0,1.0,1.0,1.0,1.0};
+float pitches[SEQUENCER_TOTAL] = {1.0,1.0,1.0,1.0,1.0,1.0};
+float decays[SEQUENCER_TOTAL] = {1.0,1.0,1.0,1.0,1.0,1.0};
+float volumes[SEQUENCER_TOTAL] = {1.0,1.0,1.0,1.0,1.0,1.0};
 
 // 画面周り
 #define MENU_MAX (18+6)
@@ -95,112 +95,12 @@ SettingMenu set = {
         SettingItemF(0.1, 1.0, 0.05, &volumes[5], "BD Volume: %4.2f", NULL, 0),
     }};
 
+static PatternSeq seq;
 static EdgeChecker clockEdge;
 static uint8_t clockGate = 0;
 static int16_t clockCount = 0;
 // 1小節4打の16ステップを4つ=16小節
-#define STEP_MAX 16
-static int16_t resetCount = STEP_MAX * 4;
-#define BEATS_TOTAL 41
-static int16_t beatsSelects[VOICE_MAX] = {1,16,29,27,25,32};
-// 0:休 1:打
-// 1以降:4小節単位での休打切り替え。例：0b1010(10)→8小節目、16小節目
-static uint8_t beats[BEATS_TOTAL][STEP_MAX]
-{
-    {1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1}, // 0
-    {1, 0, 1, 0,  1, 0, 1, 0,  1, 0, 1, 0,  1, 0, 1, 0},
-    {1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0},
-    {0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0},
-    {0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0},
-    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0},
-    {0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},
-    {0, 1, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},
-    {0, 0, 0, 0,  0, 0, 0, 1,  0, 1, 0, 0,  0, 0, 0, 0},
-    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 1, 0, 1},
-
-    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0}, // 10
-    {1, 0, 1, 0,  1, 0, 1,10,  1,10, 1, 0,  1, 0, 1, 0},
-    {1, 0, 1, 0,  1, 0, 1, 0,  1, 0, 1, 8,  1, 8, 1, 8},
-    {0, 0, 1, 0,  0, 0, 1, 0,  0,10, 1, 0,  0, 8, 1, 0},
-    {0, 8, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  2, 0, 1, 0},
-    {0, 0, 1, 0,  0, 1, 0, 8,  0, 0, 1, 0,  0, 8, 1, 0},
-    {0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1,10},
-    {1, 0, 1,10,  1, 0,10, 1,  0, 1, 0, 0,  1, 8, 0, 1},
-    {0, 1, 0, 1,  0, 1, 0, 1,  0, 1, 0, 1,  0, 1,10, 1},
-    {0, 1, 0, 1,  1,10, 0, 1,  0, 1, 1, 0,  1, 8, 0, 1},
-
-    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0}, // 20
-    {0, 0, 0, 0,  1, 0, 0, 1,  0, 1, 0, 0,  1, 0, 0, 0},
-    {0, 0, 0, 0,  1, 0, 0, 1,  0, 8, 0, 0,  1, 0, 0, 0},
-    {0, 0, 0, 0,  1, 0, 0, 1,  0, 8, 0, 0,  1, 0, 0,10},
-    {0, 8, 0, 0,  1, 0, 0,10,  0, 8, 0, 0,  1, 0, 0, 0},
-    {0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0,10},
-    {0, 8, 0, 0,  0, 0, 8, 0,  0, 8, 0, 0,  0, 0,10, 0},
-    {0, 0,10, 0,  0, 8,10, 0,  0, 8, 0,10,  2, 8, 0,10},
-    {0, 0, 0, 0,  0, 0, 0,10,  0,10,10, 0,  0, 0, 0,10},
-    {0, 8, 0, 0,  0, 0, 0,10,  8,10, 0, 0,  0, 0, 8,10},
-
-    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0}, // 30
-    {1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0,10},
-    {1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 8, 0},
-    {1, 0, 1, 0,  0, 0, 1, 0,  0, 0, 0, 1,  0, 0,10, 0},
-    {1, 0, 8, 0,  0, 0, 0, 0,  0, 0, 1, 0,  0,10, 0, 0},
-    {1, 0,10, 0,  0, 0, 0, 0,  0, 0, 1, 8,  0, 0, 0, 0},
-    {1, 0, 0, 1,  0, 8, 0, 0,  0, 0, 1, 0,  0,10, 0, 8},
-    {1, 0, 0, 1,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0,10, 0},
-    {1, 0, 0, 1,  1, 0, 1, 0,  1, 0, 1, 0,  0, 0,10, 8},
-    {1, 0, 0, 1,  0, 0, 1, 0,  0, 1, 0, 0,  1, 0, 0, 8},
-
-    {1, 0, 1, 0,  0, 1, 0, 0,  1, 0, 0, 1,  0, 0, 1, 8}, // 40
-};
-
-void updateDisplay(U8G2 *pU8g2, int8_t currentStep, int8_t selector, int8_t mode)
-{
-    static char disp_name[VOICE_MAX][3] = { "RC", "HH", "LT", "RC", "SD", "BD"};
-    uint8_t origin_x = 15;
-    uint8_t origin_y = 15;
-    uint8_t seqXStep = 7;
-    uint8_t seqYStep = 8;
-
-    pU8g2->setFont(u8g2_font_7x14B_tf);
-    pU8g2->drawStr(0, 0, "PATTERN SEQ");
-    pU8g2->setFont(u8g2_font_5x8_tf);
-
-    for (int8_t i = 0; i < 6 + 1; ++i)
-    {
-        pU8g2->drawStr(0, origin_y + (seqYStep * i), disp_name[i]);
-        pU8g2->drawHLine(origin_x, origin_y + (seqYStep * i), 127);
-    }
-
-    for (int8_t i = 0; i < STEP_MAX + 1; ++i)
-    {
-        pU8g2->drawVLine(origin_x + (seqXStep * i), origin_y, 63);
-    }
-
-    for (int8_t x = 0; x < STEP_MAX; ++x)
-    {
-        for (int8_t y = 0; y < 6; ++y)
-        {
-            uint8_t beat = beats[beatsSelects[y]][x];
-            if((beat >> 1) & 1) beat = 2;
-            else if((beat >> 2) & 1) beat = 3;
-            else if((beat >> 3) & 1) beat = 4;
-            int8_t size = 6 - beat;
-            int8_t offset = 1;
-            if (size < 6)
-                pU8g2->drawBox((origin_x + offset) + (seqXStep * x), 
-                            (origin_y + offset) + (seqYStep * y), 
-                            size, size);
-        }
-    }
-
-    pU8g2->drawBox(origin_x + (seqXStep * currentStep), origin_y - 2, seqXStep, 4);
-    
-    if (mode == 0)
-        pU8g2->drawBox(10, (origin_y + 1) + (seqYStep * (selector % 6)), 4, 6);
-    else
-        pU8g2->drawStr(10, (origin_y + 1) + (seqYStep * (selector % 6)), ">");
-}
+static int16_t resetCount = STEP_TOTAL;
 
 void initOLED()
 {
@@ -215,7 +115,7 @@ void dispOLED()
     u8g2.clearBuffer();
 
     if (menuIndex < 6) {
-        updateDisplay(&u8g2, clockCount & 0xF, menuIndex, encMode);
+        seq.updateDisplay(&u8g2, clockCount & (STEP_MAX - 1), menuIndex, encMode);
         u8g2.sendBuffer();
         return;
     }
@@ -243,7 +143,7 @@ void interruptPWM()
         clockCount = clockCount % resetCount;
     }
 
-    for (int i = 0; i < VOICE_MAX; ++i)
+    for (int i = 0; i < SEQUENCER_TOTAL; ++i)
     {
         pwm_set_gpio_level(pwmOuts[i], pKit[i]->updateWave());
     }
@@ -272,6 +172,9 @@ void setup()
     cv2.init(CV2);
     clockEdge.init(GATE);
 
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+
     initPWM(OUT1, PWM_RESO);
     initPWM(OUT2, PWM_RESO);
     initPWM(OUT3, PWM_RESO);
@@ -279,38 +182,45 @@ void setup()
     initPWM(OUT5, PWM_RESO);
     initPWM(OUT6, PWM_RESO);
 
-    initPWMIntr(PWM_INTR_PIN, interruptPWM, &interruptSliceNum, SAMPLE_FREQ, INTR_PWM_RESO, CPU_CLOCK);
+    pKit[0] = &RC;
+    pKit[1] = &OH;
+    pKit[2] = &LT;
+    pKit[3] = &RS;
+    pKit[4] = &SD;
+    pKit[5] = &BD;
+    seq.setPatternName(0, "RC");
+    seq.setPatternName(1, "HH");
+    seq.setPatternName(2, "LT");
+    seq.setPatternName(3, "RM");
+    seq.setPatternName(4, "SD");
+    seq.setPatternName(5, "BD");
+    seq.setPattern(0, 1);
+    seq.setPattern(1, 16);
+    seq.setPattern(2, 29);
+    seq.setPattern(3, 27);
+    seq.setPattern(4, 25);
+    seq.setPattern(5, 32);
 
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
+    initPWMIntr(PWM_INTR_PIN, interruptPWM, &interruptSliceNum, SAMPLE_FREQ, INTR_PWM_RESO, CPU_CLOCK);
 }
 
 void loop()
 {
+    bool withoutAcc = encMode && menuIndex >= SEQUENCER_TOTAL ? false : true;
+    
     pot.analogReadDropLow4bit();
-    enc.getDirection(true);
+    enc.getDirection(withoutAcc);
     uint16_t voct = vOct.analogReadDirect();
     int16_t cv1Value = cv1.analogReadDirect();
     uint16_t cv2Value = cv2.analogReadDirect();
 
-    // static uint8_t probabilities[4] = {0};
-    // static uint8_t states[VOICE_MAX] = {0};
-    // if ((clockCount & 0xF) == 0)
-    // {
-    //     randomSeed(voct);
-    //     for (int i = 0; i < 4; ++i)
-    //     {
-    //         probabilities[i] = random(4);
-    //     }
-    // }
-
-    for (int i = 0; i < VOICE_MAX; ++i)
+    for (int i = 0; i < SEQUENCER_TOTAL; ++i)
     {
         pKit[i]->setSpeed(pitches[i]);
         pKit[i]->updateDecay(decays[i]);
         pKit[i]->setVolume(volumes[i]);
         int state = clockGate;
-        int beat = beats[beatsSelects[i]][clockCount & 0xF];
+        int beat = seq.getBeat(i, clockCount);
         if (i == 4 && isSDFill)
         {
             beat = 1;
@@ -332,7 +242,7 @@ void loop()
     }
 
     // カウンタリセット時点灯
-    gpio_put(LED1, (clockCount & 0xF) == 0 ? HIGH : LOW);
+    gpio_put(LED1, (clockCount & (STEP_MAX - 1)) == 0 ? HIGH : LOW);
     gpio_put(LED2, clockCount == 0 ? HIGH : LOW);
 
     // static uint8_t dispCount = 0;
@@ -349,7 +259,8 @@ void loop()
     // Serial.println();
     // }
 
-    sleep_ms(1);
+    sleep_us(200);
+    // sleep_ms(1);
 }
 
 void setup1()
@@ -384,18 +295,18 @@ void loop1()
         clockCount = resetCount - 1;
     }
 
-    if (menuIndex < 6) {
+    if (menuIndex < SEQUENCER_TOTAL) {
         if (encMode)
         {
-            beatsSelects[(menuIndex % 6)] = constrain(beatsSelects[(menuIndex % 6)] + encValue, 0, BEATS_TOTAL - 1);
+            seq.addSelectPattern((menuIndex % SEQUENCER_TOTAL), encValue);
         }
     }
     else
     {
-        requiresUpdate |= set.items[menuIndex - 6].add(encValue);
+        requiresUpdate |= set.items[menuIndex - SEQUENCER_TOTAL].add(encValue);
     }
 
-    int8_t muteIndex = map(potValue, 0, 4095, 0, VOICE_MAX);
+    int8_t muteIndex = map(potValue, 0, 4095, 0, SEQUENCER_TOTAL);
     int8_t mutes[] = {-1, 0, 2, 3, 4, 1, 5};
     for (int i = 0; i < 7; ++i)
     {
